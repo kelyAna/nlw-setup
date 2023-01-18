da39ac508888fc407f82ec7f3a7b2321e6da222a
kelyAna <kely.lopes@vtex.com.br>
2023-01-17 22:22:02 -0300

    feat: add Header component

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,20 @@
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"]
+
+export const SummaryTable = () => {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, index) => {
+          return (
+            <div
+              key={`${weekDay}-${index}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
