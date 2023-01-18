f1b352b08e67f882143650dd7bf462b7709f5e1b
kelyAna <kely.lopes@vtex.com.br>
2023-01-17 21:45:41 -0300

    feat: create routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,75 @@
-import { prisma } from "./lib/prisma"
+import dayjs from 'dayjs'
+import { z } from 'zod'
 import { FastifyInstance } from 'fastify'
+import { prisma } from './lib/prisma'
 
-export function appRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const habits = await prisma.habit.findMany()
-    
-    return habits
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(
+        z.number().min(0).max(6)
+      ),
+    })
+
+    const { title, weekDays } = createHabitBody.parse(request.body)
+
+    const today = dayjs().startOf('day').toDate()
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        HabitWeekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            }
+          }),
+        }
+      }
+    })
+  })
+
+  app.get('/day', async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    })
+
+    const { date } = getDayParams.parse(request.query)
+
+    const parsedDate = dayjs(date).startOf('day')
+    const weekDay = parsedDate.get('day')
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        HabitWeekDays: {
+          some: {
+            week_day: weekDay,
+          }
+        }
+      },
+    })
+
+    const day = await prisma.day.findFirst({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabit: true,
+      }
+    })
+
+    const completedHabits = day?.dayHabit.map(dayHabit => {
+      return dayHabit.habit_id
+    })
+
+    return {
+      possibleHabits,
+      completedHabits,
+    }
   })
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,14 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import { appRoutes } from "./routes"
 
 const app = Fastify()
 
 app.register(cors)
+app.register(appRoutes)
 
 app.listen({
-  port: 3333
+  port: 3333,
 }).then(() => {
-  console.log('HTTP server running')
+  console.log('HTTP Server running!')
 })
