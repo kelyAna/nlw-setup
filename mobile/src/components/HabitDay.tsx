929d700f1b6213708d815e18ae991bfe6ca7ad1a
kelyAna <kely.lopes@vtex.com.br>
2023-01-18 23:10:49 -0300

    feat: add navigation

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -10,7 +10,7 @@ export const DAY_SIZE =
 export function HabitDay() {
   return (
     <TouchableOpacity
-      className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+      className="bg-zinc-800 rounded-lg border-2 m-1 border-zinc-800"
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
       activeOpacity={0.7}
     ></TouchableOpacity>
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -10,13 +10,13 @@ const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length
 
 export function Home() {
   return (
-    <View className="flex-1 bg-background px-8 pt-16">
+    <View className="flex-1 bg-slate-900 px-8 pt-16">
       <Header />
       <View className="flex-row mt-6 mb-2">
         {weekDays.map((weekDay, index) => (
           <Text
             key={`${weekDay}-${index}`}
-            className="text-zinc-400 text-xl font-bold text-center mx-1"
+            className="text-violet-400 text-xl font-bold text-center mx-1"
             style={{ width: DAY_SIZE }}
           >
             {weekDay}
@@ -37,7 +37,7 @@ export function Home() {
             Array.from({ length: amountOfDaysToFill }).map((_, index) => (
               <View
                 key={index}
-                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                className="bg-zinc-800 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
                 style={{ width: DAY_SIZE, height: DAY_SIZE }}
               />
             ))}
